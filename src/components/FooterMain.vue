7571ca61d87dd4987adb76834e8ba125e012c1df
pyroYV <61555067+pyroYV@users.noreply.github.com>
2022-07-16 14:35:54 +0200

    logo and position

--- src/components/FooterMain.vue
+++ src/components/FooterMain.vue
@@ -1,6 +1,9 @@
 <template>
   <footer>
     <FooterList/>
+    <div class="position-absolute" id="logo-bg">
+    <img src="../assets/img/dc-logo-bg.png" alt="dclogo">
+    </div>
   </footer>
 </template>
 
@@ -22,8 +25,18 @@ footer{
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    max-height: 450px;
+    max-height: 400px;
     padding-left:50px;
+    position:relative;
+      overflow: hidden;
+    #logo-bg{
+      right: 160px;
+      top:-50px;
+      
+      img{
+        height: 400px;
+      }
+    }
 
 }
 
